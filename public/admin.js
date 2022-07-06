3e7318be91a6fa34126db58bfddf9bcd401730bf
Matthew Herrera <why.mherrera@gmail.com>
2022-07-06 15:04:08 -0700

    done

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,29 @@
+async function gettingBooks(){
+    let response = await fetch('http://localhost:3001/listBooks')
+    let book = await response.json()
+    book.forEach(appendContent)
+}
 
-// Your Code Here
+function appendContent() {
+    let root = document.querySelector('#root')
+    let li = document.createElement('li')
+    let input = document.createElement('input')
+    quantityInput.value = book.quantity
+    let saveButton = document.createElement('button')
+    saveButton.textContent = 'Save'
+     saveButton.addEventListener('click', () => {
+        fetch('http://localhost:3001/updateBook', {
+            method: "PATCH", 
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                id: book.id,
+                quantity: quantityInput.value,
+            })
+        })
+     })
+     li.append(quantityInput, saveButton)
+     root.append(li)
+}
+gettingBooks();
